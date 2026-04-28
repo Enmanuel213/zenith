a499deb57365c802837cb00e57ef530c0d0e08a2
Enmanuel213 <enmanuelb2009@hotmail.com>
2026-04-28 07:25:23 -0400

    Primer subida

--- problematicas.js
+++ problematicas.js
@@ -1,19 +1,17 @@
 function openVideo() {
     const modal = document.getElementById("videoModal");
-    const iframe = document.getElementById("videoFrame");
+    const video = document.getElementById("videoFrame");
 
     modal.style.display = "flex";
-
-    iframe.src = "https://drive.google.com/file/d/1plBrswB2qZ7p-rld0G4Ujwf1mXTgIbJ_/preview?autoplay=1";
+    video.src = "https://www.youtube.com/embed/mV4jf536740?autoplay=1";
 }
 
 function closeVideo() {
     const modal = document.getElementById("videoModal");
-    const iframe = document.getElementById("videoFrame");
+    const video = document.getElementById("videoFrame");
 
     modal.style.display = "none";
-
-    iframe.src = "";
+    video.src = "";
 }
 // SCROLL SUAVE (esto sí estaba bien, lo dejo limpio)
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
